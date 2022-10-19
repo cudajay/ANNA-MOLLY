525d0f7c7aba0e1a7b053df1412de51cbd6705e4
jMaine <juanjose@smu.edu>
2022-10-18 23:17:45 -0500

    dvc update

--- model_utils/.ipynb_checkpoints/Model_factory-checkpoint.py
+++ model_utils/.ipynb_checkpoints/Model_factory-checkpoint.py
@@ -1,18 +1,20 @@
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
 from keras.models import Sequential
 
 class Model_Factory():
     def __init__(self):
         pass
-    def create_model(self, config):
+    def create_model(self, config, n_features):
         model = None
         if config.type  == 'LSTM':
             model = Sequential()
 
             model.add(LSTM(
                 config.layers[0],
-                input_shape=(None, 55),
+                input_shape=(None, n_features),
                 return_sequences=True))
             model.add(Dropout(config.dropout))
 
@@ -24,4 +26,23 @@ class Model_Factory():
             model.add(Dense(
                 config.n_predictions))
             model.add(Activation('linear'))
+    
+        if config.type  == 'CNN-LSTM':
+            model = Sequential()
+            model.add(Conv1D(filters=64,
+                             kernel_size=3,
+                             activation='relu',
+                             input_shape=(None, n_features)))
+            model.add(Conv1D(filters=64,
+                             kernel_size=3,
+                             activation='relu'))
+            model.add(MaxPooling1D(pool_size=2))
+            model.add(Flatten())
+            model.add(RepeatVector(n_outputs))
+            model.add(LSTM(200, activation='relu',
+                           return_sequences=True))
+            model.add(TimeDistributed(Dense(100,
+                                            activation='relu')))
+            model.add(TimeDistributed(Dense(1)))
+            model.compile(loss='mse', optimizer='adam')
         return model
